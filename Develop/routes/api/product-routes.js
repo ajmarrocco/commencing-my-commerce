d8c20558ba8e8ba360df5e64a8d1cd33b4e5f8ac
ajmarrocco <ajmarrocco@gmail.com>
2022-03-11 19:37:36 -0500

    create all api routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -108,6 +108,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
